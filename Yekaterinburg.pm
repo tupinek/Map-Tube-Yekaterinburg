1cbe008c69630db349d71e505fdc3b65ad4912e5
Michal Špaček <tupinek@gmail.com>
2015-02-17 20:43:10 +0100

    Add EXAMPLE3 section in doc.

--- Yekaterinburg.pm
+++ Yekaterinburg.pm
@@ -146,6 +146,43 @@ For more information about Yekaterinburg Map, click L<here|https://en.wikipedia.
  # Output like:
  # XML file: .*/yekaterinburg-map.xml
 
+=head1 EXAMPLE3
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Map::Tube::GraphViz;
+ use Map::Tube::GraphViz::Utils qw(node_color_without_label);
+ use Map::Tube::Yekaterinburg;
+
+ # Object.
+ my $obj = Map::Tube::Yekaterinburg->new;
+
+ # GraphViz object.
+ my $g = Map::Tube::GraphViz->new(
+         'callback_node' => \&node_color_without_label,
+         'tube' => $obj,
+ );
+
+ # Get graph to file.
+ $g->graph('Yekaterinburg.png');
+
+ # Print file.
+ system "ls -l Yekaterinburg.png";
+
+ # Output like:
+ # -rw-r--r-- 1 skim skim 28866 Feb 17 20:42 Yekaterinburg.png
+
+=begin html
+
+<a href="https://raw.githubusercontent.com/tupinek/Map-Tube-Yekaterinburg/master/images/ex3.png">
+  <img src="https://raw.githubusercontent.com/tupinek/Map-Tube-Yekaterinburg/master/images/ex3.png" alt="Екатеринбургский метрополитен" width="300px" height="300px" />
+</a>
+
+=end html
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
